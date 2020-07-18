4b95371cbdf18dce4e164bfa1598818d1de77391
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-18 20:02:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -19,13 +19,4 @@ end
 return new_nums
 end
 
-def square_array (nummies)
-  nummies = [9,10,16,25]
-new_nummies = []
-counter =0
-while counter < nummies.length do
-new_nummies.push(nummies[counter]**2)
-counter +=1
-end
-return new_nummies
-end
+
